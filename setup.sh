8b84191471eea6cea38331492c69ce61da492dde
Alex Lapin <lapinra@google.com>
2023-07-06 02:44:50 +0000

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+mkdir -p model/sam
+mv sam_vit_h_4b8939.pth model/sam
+pip3 install 'git+https://github.com/facebookresearch/segment-anything.git'
+pip3 install -U scikit-learn
