0935c7c219892c3b24bcae894232e84e333d685b
ALex Lapin <lapinra@gmail.com>
2023-07-08 02:21:58 +0000

    Improvements in labels and model. 87% accuracy.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ mkdir -p model/sam
 mv sam_vit_h_4b8939.pth model/sam
 pip3 install 'git+https://github.com/facebookresearch/segment-anything.git'
 pip3 install -U scikit-learn
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
